5c885e1b7de69712c2ca5a1d4f2d42c7652f8635
domci <dominik.cichon@bonprix.net>
2022-12-20 17:08:05 +0100

    initial commit. testing pychatgpt

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import subprocess
+from pychatgpt import Chat, Options
+
+
+
+##############################################################################
+# Git
+##############################################################################
+subprocess.run(['git', 'add', '.'])
+result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
+result.stdout
+
+
+##############################################################################
+# ChatGPT
+##############################################################################
+options = Options()
+
+# [New] Pass Moderation. https://github.com/rawandahmad698/PyChatGPT/discussions/103
+# options.pass_moderation = False
+
+# [New] Enable, Disable logs
+options.log = True
+
+# Track conversation
+options.track = True 
+
+# Use a proxy
+options.proxies = 'https://176.31.111.139:80'
+chat = Chat(email="hagohi@gmx.de", password="%zNSTRw6p&", options=options)
+answer = chat.ask("How are you?")
+print(answer)
